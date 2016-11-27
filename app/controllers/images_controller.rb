2958c3e5f8c41e4412f8a5dc54e9c78c78bffa65
JRABECK <JAKERABECK@GMAIL.COM>
2016-11-27 09:04:04 -0800

    changes probably

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,15 +1,12 @@
 class ImagesController < ApplicationController
 
 	  def add_image
-      @images = Image.all
-      @image = Image.new
-      if params[:album_id]
-      	@image[:album_id] = params[:album_id]
-      end
+    
+
   end
 
   def save_new_image
-  	@image = Image.new(image_url: params[:image_url], album_id: params[:album_id])
+  	@image = Image.new(image_url: params[:image_url], album_id: params[:album][:album_id])
     @image.save
     
     redirect_to "/index"
